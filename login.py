87d566cef0ff79a7f97714b4b8546dbe06ba4022
NeedNot <daltonhornor0@gmail.com>
2020-03-23 17:15:04 -0700

    fixed enter when typing in names

--- login.py
+++ login.py
@@ -7,6 +7,11 @@ from kivy.uix.textinput import TextInput
 from kivy.clock import Clock
 import requests
 from kivy.properties import StringProperty
+from kivy.uix.widget import Widget
+from pynput.keyboard import Key, Listener
+from kivy.base import EventLoop
+
+
 
 Config.set('input', 'mouse', 'mouse,disable_multitouch')
 Config.set('graphics', 'resizable', 0)
@@ -21,20 +26,30 @@ class LoginScreen(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-
 class Login(App):
-  playername = StringProperty()
-  def build(self):
-    self.playername = ''
-    return LoginScreen()
-  def enter_key():
-      app.stop()
+    playername = StringProperty()
 
+    def on_start(self):
+        EventLoop.window.bind(on_keyboard=self.hook_keyboard)
+        
+    def hook_keyboard(self, window, scancode, *_):
+        if scancode == 13:
+            #enter key
+            self.stop()
+
+    def build(self):
+
+
+        return LoginScreen()
 
 
 if __name__ =='__main__':
     Login().run()
 app = App.get_running_app()
+
+
+
+
 
 print(app.playername)
 player = requests.get('https://api.slothpixel.me/api/players'+ app.playername).json()
